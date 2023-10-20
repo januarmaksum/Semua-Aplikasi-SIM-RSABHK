2cb1c30c9d58c6ac3d36485f90caaff408ecae6d
Januar Maksum <januarmaksum@gmail.com>
2023-10-20 08:00:06 +0700

    refactor: add prop types

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,13 +1,11 @@
-/* eslint-disable react/prop-types */
+import PropTypes from "prop-types";
 import { FiArrowUpRight } from "react-icons/fi";
 import { MdMedicalServices } from "react-icons/md";
 
 export default function Card({ title, onClick, icon }) {
   return (
     <div className="card cursor-pointer" onClick={onClick}>
-      <div className="text-[#047759] text-[40px] mb-2 icons">
-        {icon || <MdMedicalServices />}
-      </div>
+      <div className="text-[#047759] text-[40px] mb-2 icons">{icon}</div>
       <h3>{title}</h3>
       <div className="go-corner" onClick={onClick}>
         <div className="go-arrow">
@@ -17,3 +15,15 @@ export default function Card({ title, onClick, icon }) {
     </div>
   );
 }
+
+Card.propTypes = {
+  title: PropTypes.string,
+  icon: PropTypes.element,
+  onClick: PropTypes.func,
+};
+
+Card.defaultProp = {
+  title: "Title",
+  icon: <MdMedicalServices />,
+  onClick: undefined,
+};
